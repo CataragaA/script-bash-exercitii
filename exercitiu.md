ed7bc54f0d7cf9d9328b172234b4b3e228a5c99f
CataragaA <ana.cataraga@hotmail.com>
2021-05-01 15:25:51 +0300

    adaugarea ex 6,7,8 cu o mica eroare la ex 7 care trebuie rezolvata

--- exercitiu.md
+++ exercitiu.md
@@ -1,9 +1,11 @@
-In foderul ‘input’ avem 5 fisiere cu denumirea despre  expeditor-suma-moneda-destinatar:
+In foderul ‘input’ avem 6 fisiere cu denumirea despre  expeditor-suma-moneda-destinatar:
 
 * ana-100-eur-mara
+* ana-2-mdl-mara
+* dana-100-eur-mara
 * mara-200-ron-ana
 * dana-8765-gbp-ana
-* ana-2-mdl-mara
+* mara-200-eur-dana
 
 
 1. Scrie intr-un fisier nou creat, numit ’sume_trimise_ana’, toate sumele  trimise de ana. 
@@ -14,4 +16,10 @@ In foderul ‘input’ avem 5 fisiere cu denumirea despre  expeditor-suma-moneda
 
 4. Scrie intr-un fisier nou creat, ‘lista_sume_monede_primite_ana’, toate sumele si monedele primite de ana
 
-5. Scrie intr-un fisier nou creat, ‘info_sume_monede_primite_ana’, numele destinatarului si al expeditorului
+5. Scrie intr-un fisier nou creat, ‘info_sume_monede_primite_ana’, numele expeditorului si destinatarului.
+
+6. Scrie intr-un fisier nou creat  ‘lista_monede’, lista tuturor monedelor folosite. 
+7. Scrie intr-un fisier "drepturi_fisiere", drepturile fiecarui fisier de input, urmat de numele fisierului.
+8. Gaseste fisierele in care se fac transferuri de 100 si 200 folosind doua metode: 
+    * cu comanda find si te ajuti de comanda grep.
+    * folosind expresie regulata ca argument pentru find (find cu expresie regulata, o sa folosesti -regex in loc de -name)
--- moneda.sh
+++ moneda.sh
@@ -1,38 +1,58 @@
 #!/bin/bash
+rm -rf output/
 mkdir -p output
 
-echo "Scrie intr-un fisier nou creat, numit ’sume_trimise_ana’, toate sumele  trimise de ana."
+echo "1.Scrie intr-un fisier nou creat, numit ’sume_trimise_ana’, toate sumele  trimise de ana."
 find . -name "ana*" | cut -d - -f 2 > output/sume_trimise_ana.txt
 echo " ===Rezultat obtinut==="
 cat output/sume_trimise_ana.txt
 echo "=========="
 echo -e "\n"
 
-echo "Scrie intr-un fisier nou creat, numit ‘lista_sume_monede_trimise_ana’, toate sumele si monedele trimise de ana."
+echo "2.Scrie intr-un fisier nou creat, numit ‘lista_sume_monede_trimise_ana’, toate sumele si monedele trimise de ana."
 find . -name "ana*" | cut -d - -f 2,3 > output/lista_sume_monede_trimise_ana.txt
 echo " ===Rezultat obtinut==="
 cat output/lista_sume_monede_trimise_ana.txt
 echo "=========="
 echo -e "\n"
 
-echo "Scrie intr-un fisier nou creat, numit ‘sume_monede_trimise_nu_ana’, toate sumele si monedele care sunt trimise de altcineva decat ana."
+echo "3.Scrie intr-un fisier nou creat, numit ‘sume_monede_trimise_nu_ana’, toate sumele si monedele care sunt trimise de altcineva decat ana."
 find ./input/ -type f ! -name "ana*" | cut -d - -f 2,3 > output/sume_monede_trimise_nu_ana.txt
 echo " ===Rezultat obtinut==="
 cat output/sume_monede_trimise_nu_ana.txt
 echo "=========="
 echo -e "\n"
 
-echo "Scrie intr-un fisier nou creat, ‘lista_sume_monede_primite_ana’, toate sumele si monedele primite de ana."
-find ./input -name "*ana.txt" | cut -d - -f 2,3 > output/lista_sume_monede_primite_ana.txt
+echo "4.Scrie intr-un fisier nou creat, ‘lista_sume_monede_primite_ana’, toate sumele si monedele primite de ana."
+find ./input -name "*-ana.txt" | cut -d - -f 2,3 > output/lista_sume_monede_primite_ana.txt
 echo " ===Rezultat obtinut==="
 cat output/lista_sume_monede_primite_ana.txt
 echo "=========="
 echo -e "\n"
 
-echo "Scrie intr-un fisier nou creat, ‘info_sume_monede_primite_ana’, numele destinatarului si al expeditorului."
-find ./input -name "*ana.txt" | cut -d - -f -1,4- | cut -d / -f 3 | cut -d . -f 1 > output/info_sume_monede_primite_ana.txt
+echo "5.Scrie intr-un fisier nou creat, ‘info_sume_monede_primite_ana’, numele expeditorului si al destinatarului."
+find ./input -name "*-ana.txt" | cut -d - -f 1,4 | cut -d / -f 3 | cut -d . -f 1 | tr "-" "," > output/info_sume_monede_primite_ana.txt
 echo " ===Rezultat obtinut==="
 cat output/info_sume_monede_primite_ana.txt
 echo "=========="
 echo -e "\n"
+
+echo "6.Scrie intr-un fisier nou creat  ‘lista_monede’, lista tuturor monedelor folosite."
+ls -1 input/ | cut -d - -f 3 > output/lista_monede.txt
+echo " ===Rezultat obtinut==="
+cat output/lista_monede.txt
+echo "=========="
+echo -e "\n"
+
+
+echo "7.Scrie intr-un fisier "drepturi_fisiere", drepturile fiecarui fisier de input, urmat de numele fisierului."
+ls -l input/ | cut -d " " -f 1,9 > output/drepturi_fisiere.txt
+echo " ===Rezultat obtinut==="
+cat output/drepturi_fisiere.txt
+echo "=========="
+echo -e "\n"
+
+echo "8.Gaseste fisierele in care se fac transferuri de 100 si 200."
+find ./input/ -type f -name "*100*" -o -name "*200*"
+echo "=========="
 
